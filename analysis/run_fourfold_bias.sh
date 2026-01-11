c00c07097aaeb5dc52fd028eacc6fdaf050f3437
Alessandro Oneto <122585245+Alebraco@users.noreply.github.com>
2026-01-11 15:53:36 -0500

    Create run_fourfold_bias.sh

---
+++ analysis/run_fourfold_bias.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#BSUB -J fourfold_bias
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -n 1
+#BSUB -R "rusage[mem=16GB] span[hosts=1]"
+#BSUB -W 24:00
+#BSUB -q bobay
+
+source ~/.bashrc
+conda activate /usr/local/usrapps/metastrain/asoneto/annotation/
+python3 -u scripts_endosymb/analysis/fourfold_bias.py
