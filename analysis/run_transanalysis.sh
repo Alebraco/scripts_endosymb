9ef6bde45292a81b87c22fb963dd81e35f619faa
Alessandro Oneto <122585245+Alebraco@users.noreply.github.com>
2026-01-30 13:47:58 -0500

    Create run_transanalysis.sh

---
+++ analysis/run_transanalysis.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#BSUB -J spectrum
+#BSUB -o %J.out
+#BSUB -e %J.err
+#BSUB -n 1
+#BSUB -R "rusage[mem=8GB] span[hosts=1]"
+#BSUB -W 24:00
+#BSUB -q bobay
+
+source ~/.bashrc
+conda activate /usr/local/usrapps/metastrain/asoneto/annotation/
+python3 scripts_endosymb/analysis/transposase_analysis.py
