4e13d2d3a1fa0f5dc2bc88ccaace90b24df019e7
Alessandro Oneto <122585245+Alebraco@users.noreply.github.com>
2026-02-17 16:02:17 -0500

    Update spectrum.py

--- analysis/spectrum.py
+++ analysis/spectrum.py
@@ -583,15 +583,15 @@ def rate_shift_plot(df, position, color_by = 'distance', annotate=False):
 
         cbar = plt.colorbar(scatter)
         cbar.set_label(cbar_label, rotation=270, labelpad=25, fontsize=12)
-        cbar.ax.tick_params(labelsize=12)
+        cbar.ax.tick_params(labelsize=16)
         
         plt.axline((0, 0), slope=1, color='gray', linestyle='--', linewidth=1, label='y=x (No shift)')
         plt.xlim(-0.05, 1.05)
         plt.ylim(-0.05, 1.05)
 
-        plt.title(f'{mut.replace("r","")} Rate Shift\n{site_label}', fontsize=22)
-        plt.xlabel('Free-Living Relatives', fontsize=18)
-        plt.ylabel('Endosymbionts', fontsize=18)
+        plt.title(f'{mut.replace("r","")} Rate Shift\n{site_label}', fontsize=22, fontweight='bold')
+        plt.xlabel('Free-Living Relatives', fontsize=18, fontweight='bold')
+        plt.ylabel('Endosymbionts', fontsize=18, fontweight='bold')
         plt.tick_params(axis='both', labelsize=16)
         plt.legend(fontsize=16)
         plt.tight_layout()
