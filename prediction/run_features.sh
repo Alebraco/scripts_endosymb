8c99a20d1a0d8e044c530b1b9bb9fa6d4aba3dfe
Alessandro Oneto <122585245+Alebraco@users.noreply.github.com>
2026-03-02 12:52:08 -0500

    Update run_features.sh

--- prediction/run_features.sh
+++ prediction/run_features.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #BSUB -n 1
 #BSUB -W 100:00
-#BSUB -R "rusage[mem=4GB] span[hosts=1]"
+#BSUB -R "rusage[mem=8GB] span[hosts=1]"
 #BSUB -J collect_features
 #BSUB -o %J.out
 #BSUB -e %J.err
 
-python collect_features.py
+python scripts_endosymb/prediction/collect_features.py
