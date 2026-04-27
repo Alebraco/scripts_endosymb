a552940a76b76f264cdf4c67f68761a1805b200e
Alessandro Oneto <122585245+Alebraco@users.noreply.github.com>
2026-04-27 12:08:18 -0400

    Create run_theta_corr.sh

---
+++ analysis/run_theta_corr.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#BSUB -J theta_corr
+#BSUB -W 1:00
+#BSUB -q bobay
+#BSUB -e %J.err
+#BSUB -o %J.out
+#BSUB -R "rusage[mem=8GB] span[hosts=1]"
+#BSUB -n 1
+
+source ~/.bashrc
+conda activate /usr/local/usrapps/metastrain/asoneto/annotation
+
+python3 -u scripts_endosymb/analysis/theta_correlations.py
